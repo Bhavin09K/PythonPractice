49fd72869f5a3c7b10c886241d7e464dab8bd392
Bhavin09K <100960558+Bhavin09K@users.noreply.github.com>
2022-04-04 22:45:16 +0530

    Add files via upload

---
+++ # Methods of Data Structures.py
@@ -0,0 +1,51 @@
+#For List
+list.append()
+list.clear()
+list.copy()
+list.index()
+list.extend()
+list.sort()
+list.pop()
+list.remove()
+list.count()
+list.reverse()
+list.insert()
+
+# For Dictionary
+dict.clear()
+dict.copy()
+dict.fromkeys()
+dict.get()
+dict.items()
+dict.pop()
+dict.values()
+dict.update()
+dict.setdefault()
+dict.popitem()
+dict.keys()
+
+
+#For Tuples
+
+tuple.count()
+tuple.index()
+
+#For Sets
+
+set.add()
+set.difference()
+set.clear()
+set.copy()
+set.difference_update()
+set.discard()
+set.intersection()
+set.intersection_update()
+set.isdisjoint()
+set.issubset()
+set.issuperset()
+set.pop()
+set.remove()
+set.symmetric_difference()
+set.symmetric_difference_update()
+set.union()
+set.update()
