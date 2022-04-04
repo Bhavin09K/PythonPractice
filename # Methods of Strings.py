e4b23118489248bb84375583817a9fe5cb49bf2a
Bhavin09K <100960558+Bhavin09K@users.noreply.github.com>
2022-04-04 22:45:34 +0530

    Add files via upload

---
+++ # Methods of Strings.py
@@ -0,0 +1,49 @@
+# Methods of Strings
+
+from tokenize import String
+
+
+String.capitalize()	#Converts the first character to upper case
+String.casefold()	#Converts string into lower case
+String.center()	#Returns a centered string
+String.count()	#Returns the number of times a specified value occurs in a string
+String.encode()	#Returns an encoded version of the string
+String.endswith()	#Returns true if the string ends with the specified value
+String.expandtabs()	#Sets the tab size of the string
+String.find()	#Searches the string for a specified value and returns the position of where it was found
+String.format()	#Formats specified values in a string
+String.format_map()	#Formats specified values in a string
+String.index()	#Searches the string for a specified value and returns the position of where it was found
+String.isalnum()	#Returns True if all characters in the string are alphanumeric
+String.isalpha()	#Returns True if all characters in the string are in the alphabet
+String.isdecimal()	#Returns True if all characters in the string are decimals
+String.isdigit()	#Returns True if all characters in the string are digits
+String.isidentifier()	#Returns True if the string is an identifier
+String.islower()	#Returns True if all characters in the string are lower case
+String.isnumeric()	#Returns True if all characters in the string are numeric
+String.isprintable()	#Returns True if all characters in the string are printable
+String.isspace()	#Returns True if all characters in the string are whitespaces
+String.istitle()	#Returns True if the string follows the rules of a title
+String.isupper()	#Returns True if all characters in the string are upper case
+String.join()	#Joins the elements of an iterable to the end of the string
+String.ljust()	#Returns a left justified version of the string
+String.lower()	#Converts a string into lower case
+String.lstrip()	#Returns a left trim version of the string
+String.maketrans()	#Returns a translation table to be used in translations
+String.partition()	#Returns a tuple where the string is parted into three parts
+String.replace()	#Returns a string where a specified value is replaced with a specified value
+String.rfind()	#Searches the string for a specified value and returns the last position of where it was found
+String.rindex()	#Searches the string for a specified value and returns the last position of where it was found
+String.rjust()	#Returns a right justified version of the string
+String.rpartition()	#Returns a tuple where the string is parted into three parts
+String.rsplit()	#Splits the string at the specified separator, and returns a list
+String.rstrip()	#Returns a right trim version of the string
+String.split()	#Splits the string at the specified separator, and returns a list
+String.splitlines()	#Splits the string at line breaks and returns a list
+String.startswith()	#Returns true if the string starts with the specified value
+String.strip()	#Returns a trimmed version of the string
+String.swapcase()	#Swaps cases, lower case becomes upper case and vice versa
+String.title()	#Converts the first character of each word to upper case
+String.translate()	#eturns a translated string
+String.upper()	#Converts a string into upper case
+String.zfill()	#Fills the string with a specified number of 0 values at the beginning
